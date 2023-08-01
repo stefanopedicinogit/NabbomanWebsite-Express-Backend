506f22586deaec53dfdcf1a414abd770275102ff
Stefano Pedicino <ss_pedi@outlook.com>
2023-08-01 18:38:18 +0200

    fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "http://nabbomanwebsite.netlify.app",
+    origin: ["http://nabbomanwebsite.netlify.app", "localhost:3000"],
   })
 )
 app.use(express.json({ limit: '50mb' }));
